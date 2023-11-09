4ca105d59389faa52bcaa8afaed703dc5ba470cb
rumitparmar <rumit.parmar2016@gmail.com>
2023-11-09 23:20:40 +0530

    done

---
+++ src/Acme.BookStore.Domain.Shared/SharedServices/AwsSecretsManagerService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System;
+using Amazon.SecretsManager.Model;
+using Amazon.SecretsManager;
+using Amazon;
+using System.Text.Json;
+using Acme.BookStore.DataTransferObjects;
+
+namespace Acme.BookStore.SharedServices
+{
+    public class AwsSecretsManagerService
+    {
+        public async Task<string> GetSecretAsync(IConfiguration configuration)
+        {
+            var environment = Environment.GetEnvironmentVariable("RUNNING_ENVIRONMENT");
+            Console.WriteLine($"Environment: {environment}");
+            if (!string.IsNullOrEmpty(environment) && environment.Equals("Local", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("Local environment detected, using local connection string");
+                Console.WriteLine(configuration.GetConnectionString("Default"));
+                return configuration.GetConnectionString("Default") ?? "";
+            }
+            string secretName = "Database-Secret";
+            string region = "us-east-1";
+            using IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+            GetSecretValueRequest request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+            };
+            GetSecretValueResponse response = await client.GetSecretValueAsync(request);
+            var secretString = response.SecretString;
+            var secrets = JsonSerializer.Deserialize<DatabaseSecrets>(secretString!);
+            await Console.Out.WriteLineAsync($"Secrets: {secretString}");
+            await Console.Out.WriteLineAsync(secrets!.GetConnectionString());
+            return secrets!.GetConnectionString();
+        }
+    }
+}
---
+++ src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContextFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+#if !DEBUG
+using Acme.BookStore.SharedServices;
+#endif
+
+namespace Acme.BookStore.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class BookStoreDbContextFactory : IDesignTimeDbContextFactory<BookStoreDbContext>
+{
+
+    public BookStoreDbContext CreateDbContext(string[] args)
+    {
+        // https://www.npgsql.org/efcore/release-notes/6.0.html#opting-out-of-the-new-timestamp-mapping-logic
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        BookStoreEfCoreEntityExtensionMappings.Configure();
+
+        var configuration = BuildConfiguration();
+#if DEBUG
+        var connectionString = configuration.GetConnectionString("Default");        
+#else
+        var connectionString = new AwsSecretsManagerService().GetSecretAsync(configuration).Result;
+#endif
+
+        var builder = new DbContextOptionsBuilder<BookStoreDbContext>()
+            .UseNpgsql(connectionString);
+
+        return new BookStoreDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Acme.BookStore.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }    
+}
+
+
+
